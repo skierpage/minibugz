07f385eb770696e7e66cbec186f5bb236ab3e9a6
skierpage <info@skierpage.com>
2012-06-19 01:36:08 -0700

    add status_last_modified

--- sql/bugs.sql
+++ sql/bugs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 18, 2012 at 10:01 PM
+-- Generation Time: Jun 19, 2012 at 01:35 AM
 -- Server version: 5.5.24
 -- PHP Version: 5.3.10-1ubuntu3.1
 
@@ -30,10 +30,12 @@ CREATE TABLE IF NOT EXISTS `bugs` (
   `bug_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varbinary(255) NOT NULL,
   `description` varbinary(1000) NOT NULL,
-  `status_id` tinyint(3) NOT NULL DEFAULT '1' COMMENT 'see status_code',
+  `status_id` tinyint(3) NOT NULL COMMENT 'see status_code',
+  `status_last_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'update to now() on status changes',
   PRIMARY KEY (`bug_id`),
   UNIQUE KEY `title` (`title`),
-  KEY `status_id` (`status_id`)
+  KEY `status_id` (`status_id`),
+  KEY `status_last_modified` (`status_last_modified`)
 ) ENGINE=InnoDB DEFAULT CHARSET=binary AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
