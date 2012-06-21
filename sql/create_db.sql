3684d9b7eb3ce25df63263265c642de2f52e87e1
skierpage <info@skierpage.com>
2012-06-20 21:46:14 -0700

    hints to run

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,4 +1,6 @@
--- Run this once, probably as root DB user
+-- Run this once, probably as root DB user, e.g. with
+-- % mysql -u root -p
+-- source create_db.sql
 create database minibugz;
 grant index, create, select, insert, update, delete, alter, lock tables on minibugz.* to 'minibugz_user'@'localhost' identified by 'pwd';
 
