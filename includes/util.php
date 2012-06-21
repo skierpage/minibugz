6399592cfd7128a126374c460e659e21ee94bd76
skierpage <info@skierpage.com>
2012-06-20 20:54:35 -0700

    No need for wantValues() with real bug object

--- includes/util.php
+++ includes/util.php
@@ -11,14 +11,3 @@ function isDebugMode() {
     }
 }
 
-/**
- * Maybe...
- * Get the values of a named set of keys from the named array.
- *
- * @param array $arr : array holding values
- * @param array $keys : array of key names to return, or one key string
- * @return null|any :  value of key or null
-function wantValues( $arr, $keys) {
-
-}
-*/
