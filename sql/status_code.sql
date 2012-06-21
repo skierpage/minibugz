46ffc8e3cc6985b56f2dd4b10c55135607b861c8
skierpage <info@skierpage.com>
2012-06-20 21:53:45 -0700

    add unique key on ordering

--- sql/status_code.sql
+++ sql/status_code.sql
@@ -31,7 +31,8 @@ CREATE TABLE IF NOT EXISTS `status_code` (
   `status_name` varbinary(30) NOT NULL,
   `ordering` tinyint(4) DEFAULT NULL COMMENT 'determines the order of active status codes',
   `active` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0 means inactive',
-  PRIMARY KEY (`status_id`)
+  PRIMARY KEY (`status_id`),
+  UNIQUE KEY `ordering` (`ordering`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
